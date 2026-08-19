9e72c7035c60cc54d2b3ad6cf0e8a303b27da959
Soubhagya Kabiraj <sou.aec.cs@gmail.com>
2026-08-19 12:53:02 +0530

    Time: 621 ms (5.04%), Space: 79.6 MB (5.25%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countPrimes(int n) {
+        boolean[] composite = new boolean[n];
+        int count = 0;
+
+        for (int i = 2; i * i < n; i++) {
+            if (!composite[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    composite[j] = true;
+                }
+            }
+        }
+
+        for (int i = 2; i < n; i++) {
+            if (!composite[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
