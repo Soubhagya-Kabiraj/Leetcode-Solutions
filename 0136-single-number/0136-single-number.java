d1b1ff2a5ecb45a203b55ca3157de9285c6a7923
Soubhagya Kabiraj <sou.aec.cs@gmail.com>
2026-08-23 11:10:52 +0530

    Time: 1 ms (99.94%), Space: 46.8 MB (65.4%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        if (nums.length == 0) return 0;
+        int ans = 0;
+        for (int num : nums) {
+            ans = ans ^ num;
+        }
+        return ans;
+    }
+}
